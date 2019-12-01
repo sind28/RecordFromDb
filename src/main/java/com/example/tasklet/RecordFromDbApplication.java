7a96596942c43a1decb23065ed3c9a0d8de72fe1
sind28 <sind2805@gmail.com>
2019-11-30 18:28:05 -0600

    first commit

---
+++ src/main/java/com/example/tasklet/RecordFromDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordFromDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordFromDbApplication.class, args);
+	}
+
+}
