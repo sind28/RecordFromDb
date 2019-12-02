e415c2fa8a05ecc8563433dd54a2cb19bc5f6962
sind28 <sind2805@gmail.com>
2019-12-01 18:03:45 -0600

    sec commit

--- src/main/java/com/example/tasklet/RecordFromDbApplication.java
+++ src/main/java/com/example/tasklet/RecordFromDbApplication.java
@@ -1,13 +1,40 @@
 package com.example.tasklet;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+
 public class RecordFromDbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecordFromDbApplication.class, args);
+		String[] springConfig  = 
+			{	
+					
+				"Job/Job.xml" 
+			};
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext(springConfig);	
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("Job");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Done");
+		
+		
 	}
 
 }
